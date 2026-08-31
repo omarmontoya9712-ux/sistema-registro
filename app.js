79a62fee3afe24ce146e8eb8d85be40dc2fb5ba2
omarmontoya9712-ux <omarmontoya9712@gmail.com>
2026-08-31 10:41:54 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const btnEntrada = document.getElementById("btnEntrada");
+const btnSalida = document.getElementById("btnSalida");
+
+btnEntrada.addEventListener("click", () => {
+    alert("Seleccionaste ENTRADA");
+});
+
+btnSalida.addEventListener("click", () => {
+    alert("Seleccionaste SALIDA");
+});
